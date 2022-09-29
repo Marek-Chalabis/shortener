9c77843f83f776ebeb8dde60c9ad453914910088
Marek-Chalabis <chalabismarek@gmail.com>
2022-09-29 19:39:09 +0200

    Fix urls
    Change serializer class to proper ViewSet one

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,10 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from api.serializers import AliasedUrlSerializer
-from api.views import resolve_aliased_url
+from api.views import resolve_aliased_url, AliasedUrlViewSet
 
 router_v1 = DefaultRouter()
-router_v1.register('aliased-url', AliasedUrlSerializer, basename='aliased-url')
+router_v1.register('aliased-url', AliasedUrlViewSet, basename='aliased-url')
 
 urlpatterns = [
     path('<str:alias>', resolve_aliased_url),
